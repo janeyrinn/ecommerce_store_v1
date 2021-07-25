ffbe75f7365f65334fa43b091a57f84564a02809
janeyrinn <jane.rinn@gmail.com>
2021-07-25 12:53:29 +0000

    Added forms, admin and signals

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+"""
+inline item admin below allows us to add and edit line items in the admin
+from inside the order model
+"""
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    # add inlines option here to add it to the order admin interface
+    inlines = (OrderLineItemAdminInline,)
+    # calc by model methods
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+    # establishes the order of the fields which would otherwise be adjusted by django
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total',)
+    # only a few key items to display
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+    # reverse chronological order displayed so most recent at the top
+    ordering = ('-date',) 
+#registers Order and Order Admin models but skip inlone as its now accessible in the order model
+admin.site.register(Order, OrderAdmin)
---
+++ checkout/signals.py
@@ -0,0 +1,22 @@
+# sends signals
+from django.db.models.signals import post_save, post_delete
+# recieves signals
+from django.dispatch import receiver
+# where the signals come from
+from .models import OrderLineItem
+
+
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Parameters ref to the sender of the signal
+    Update order total on lineitem update/create
+    """
+    instance.order.update_total()
+
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_save(sender, instance, **kwargs):
+    """
+    Update order total on lineitem delete
+    """
+    instance.order.update_total()
