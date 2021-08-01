6459a9225709bb94fc460ef65e22e74c00e438c9
janeyrinn <jane.rinn@gmail.com>
2021-08-01 18:04:09 +0000

    added check out form data caching in payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,5 @@
-
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -9,6 +9,24 @@ from products.models import Product
 from bag.contexts import bag_contents
 
 import stripe
+import json
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
+
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
@@ -65,14 +83,14 @@ def checkout(request):
                 Please double check your information.')
     else:
         bag = request.session.get('bag', {})
+        # manually prevents user accessing the url by typing /checkout
         if not bag:
-            # manually prevents user accessing the url by typing /checkout
             messages.error(request, "There's nothing in your bag at the moment")
             return redirect(reverse('products'))
 
         current_bag = bag_contents(request)
         total = current_bag['grand_total']
-        stripe_total =round(total * 100)
+        stripe_total = round(total * 100)
         stripe.api_key = stripe_secret_key
         intent = stripe.PaymentIntent.create(
             amount=stripe_total,
@@ -83,16 +101,17 @@ def checkout(request):
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
-        Did you forget to set it in your environment?')
+            Did you forget to set it in your environment?')
 
     template = 'checkout/checkout.html'
     context = {
-    'order_form': order_form,
-    'stripe_public_key': stripe_public_key,
-    'client_secret': intent.client_secret,
+        'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
+
 
 def checkout_success(request, order_number):
     """
