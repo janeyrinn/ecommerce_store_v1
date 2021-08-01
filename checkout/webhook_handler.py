0f18372fee75e473be271b99000da7c3dc67a0dc
janeyrinn <jane.rinn@gmail.com>
2021-08-01 14:46:05 +0000

    add webhook handler

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,21 @@
+"""
+Built in redundancy : prevent actions being lost if incompleted, form/order submission etc.
+every time an event occurs on stripe, stripe sends out a webhook we can listen for, they can be sent to a url we specify.
+custom class below will handle these webhooks
+"""
+from django.http import HttpResponse
+
+
+class StripeWH_Handler:
+    """Handle Stripe webhooks"""
+
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+        """
+        Handle a generic/unknown/unexpected webhook event
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
