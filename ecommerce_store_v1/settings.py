e18ea4e7920e0be39a77dec9a299148525946ffc
janeyrinn <jane.rinn@gmail.com>
2021-08-08 15:55:12 +0000

    deployment pt 1

--- ecommerce_store_v1/settings.py
+++ ecommerce_store_v1/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,7 +124,7 @@ WSGI_APPLICATION = 'ecommerce_store_v1.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
